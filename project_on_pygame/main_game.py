9558123f0f6d59df8d80c9fdd6b882a46b9bed4c
Grizzly18 <mz-100@mail.ru>
2022-01-18 20:37:32 +0300

    ffffffff

--- project_on_pygame/main_game.py
+++ project_on_pygame/main_game.py
@@ -1,21 +1,37 @@
+from socket import socket
 import pygame
 from win32api import GetSystemMetrics
 from functions import *
-from board import *
+from Socket import Socket
+import asyncio
+from threading import Thread
 
 WIDTH, HEIGHT = GetSystemMetrics(0), GetSystemMetrics(1)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 all_sprites = pygame.sprite.Group()
-functs = {None: None, 0: terminate, 1: None, 2: None}
+functs = {None: None, 0: terminate, 1: None, 2: "Login"}
 FPS = 120
+messages, online = "", False
 
+def translate(word):
+    new_word = {}
+    for i in range(len(word)):
+        new_word = f"{i};{'$'.join(word[i])} "
+    for i in range(len(word.split(" "))):
+        t = i.split(';')
+        new_word[t[0]] = t[1].split(',')
+    return new_word
 
 class MainPage:
     def __init__(self):
+        global messages, client
+        if online:
+            client.send_data("check listgame")
         self.all_page_buttons = []
         self.all_page_buttons.append(Button(load_image("create.png", colorkey=-1), (WIDTH * 0.75, HEIGHT * 0.25)))
         text = font.render(f'Ожидают игры', True, pygame.Color("black"))
         place = text.get_rect(center=(WIDTH * 0.3, HEIGHT * 0.25))
+        print(translate(messages))
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -30,13 +46,12 @@ class MainPage:
             all_sprites.update()
             clock.tick(FPS)
 
-    def ex(self):
-        for button in self.all_page_buttons:
-            button.kill()
-    
-
 class MAIN:
     def __init__(self):
+        global online
+        if client.set_up() is None:
+            online = True
+            Thread(target=client.start, daemon=True).start()
         Button(load_image("logo.png", colorkey=-1), (WIDTH / 100 * 16, HEIGHT / 100 * 7.5))
         Button(load_image("findgame.png", colorkey=-1), (WIDTH / 100 * 80, 65), 1)
         Button(load_image("login.png", colorkey=-1), (WIDTH / 100 * 93, 63), 2)
@@ -56,13 +71,47 @@ class Button(pygame.sprite.Sprite):
         if args and args[0].type == pygame.MOUSEBUTTONDOWN:
             if self.rect.collidepoint(args[0].pos):
                 if functs[self.command] != None:
-                    print(self.command)
-                    functs[self.command]()
+                    if self.command == 2:
+                        # Pages = {"Login": Login, "MainPage": MainPage}
+                        # Pages[functs[self.command]]()
+                        pass
+                    else:
+                        print(self.command)
+                        functs[self.command]()
+
+
+class Client(Socket):
+    def __init__(self):
+        super(Client, self).__init__()
+
+    def set_up(self):
+        try:
+            self.socket.connect(
+                ("127.0.0.1", 1234)
+            )
+        except ConnectionRefusedError:
+            return "Sorry, server is offline"
+
+        self.socket.setblocking(False)
+        return
+
+    async def listen_socket(self, listened_socket=None):
+        global messages
+        while True:
+            data = await self.main_loop.sock_recv(self.socket, 2048)
+            messages = data.decode('utf-8')
+
+    def send_data(self, data=None):
+        socket.send(self.socket, data.encode("utf-8"))
+
+    async def main(self):
+        await self.main_loop.create_task(self.listen_socket())
+
 
 if __name__ == "__main__":
     pygame.init()
     pygame.display.set_caption("Монополия")
     clock = pygame.time.Clock()
+    client = Client()
     font = pygame.font.Font(None, 60)
     MAIN()
-    pygame.quit()
---
+++ project_on_pygame/server.py
@@ -0,0 +1,68 @@
+from markupsafe import re
+from Socket import Socket
+import asyncio
+
+
+def translate(word):
+    new_word = ""
+    for i in range(len(word)):
+        new_word = f"{i};{'$'.join(word[i])}"
+    return new_word
+
+class Server(Socket):
+    def __init__(self):
+        super(Server, self).__init__()
+        self.games = {}
+        self.users = []
+
+    def set_up(self):
+        self.socket.bind(("127.0.0.1", 1234))
+
+        self.socket.listen(5)
+        self.socket.setblocking(False)
+        print("Server is listening")
+
+    async def send_data(self, data=None):
+        for user in self.users:
+            await self.main_loop.sock_sendall(user, data)
+
+    async def listen_socket(self, listened_socket=None):
+        if not listened_socket:
+            return
+
+        while True:
+            try:
+                data = await self.main_loop.sock_recv(listened_socket, 2048)
+                if data.decode('utf-8') == "create game":
+                    self.games[f"game-{listened_socket}"] = [listened_socket]
+                elif "join game" in data.decode('utf-8'):
+                    print(data[2][5:], data[2][4:])
+                    self.games[data[2][5:]].append(listened_socket)
+                elif "check listgame" == data.decode('utf-8'):
+                    await self.main_loop.sock_sendall(listened_socket, translate(self.games).encode('utf-8'))
+                else:
+                    await self.send_data(data)
+                print(self.games)
+
+            except ConnectionResetError:
+                print("Client removed")
+                self.users.remove(listened_socket)
+                return
+
+    async def accept_sockets(self):
+        while True:
+            user_socket, address = await self.main_loop.sock_accept(self.socket)
+            print(f"User <{address[0]}> connected!")
+
+            self.users.append(user_socket)
+            self.main_loop.create_task(self.listen_socket(user_socket))
+
+    async def main(self):
+        await self.main_loop.create_task(self.accept_sockets())
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.set_up()
+
+    server.start()
